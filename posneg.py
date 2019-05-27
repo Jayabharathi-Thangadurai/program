928fc52efdd2c47456bb7bbfbae0d675d37f2245
Jayabharathi-Thangadurai <46148143+Jayabharathi-Thangadurai@users.noreply.github.com>
2019-05-27 22:47:41 +0530

    Update posneg.py

--- posneg.py
+++ posneg.py
@@ -1,7 +1,9 @@
 n=int(input())
-if n>0:
-  print("Positive")
-elif n<0:
+if n<0:
   print("Negative")
-else:
+elif n>0:
+  print("Positive")
+elif n==0:
   print("Zero")
+else:
+  print("out of range")
